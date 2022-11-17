e7179f2c9e27751ab0441030759c464e77c0f1ba
lopeeh <113986224+lopeeh@users.noreply.github.com>
2022-11-17 12:37:14 +0100

    Update programacionModular1.py

--- programacionModular1.py
+++ programacionModular1.py
@@ -316,15 +316,15 @@ print(f"Multiplos de K: {multiplosK(lista, k)}")
 
 """
 def conversorBinarioDecimal(numero):
-    numero_decimal = 0 
-    posicion=len(numero)-2
+    numero_decimal = 0
+    posicion=len(numero)-1
 
     for i in numero:
         numero=int(i)
         numero_decimal+=numero*2**posicion
         posicion -= 1
-    
-        return numero_decimal
+
+    return numero_decimal
 
 def conversorDecimalBinario(numero):
     numeroBinario = 0
@@ -334,35 +334,40 @@ def conversorDecimalBinario(numero):
         numeroBinario = numeroBinario+numero%2*posicionBinario
         numero//=2
         posicionBinario*=10
-    
+
     return numeroBinario
 
 numero = input("Introduce un número decimal o binario(Ejemplos: 10100101b, 123d):")
 
 datoAccp = False
-if numero[len(numero)-1:len(numero)] == "b":
-    while datoAccp == False:
-        tmp = str(numero[:-1])
-        for i in range(len(tmp)):
-            if tmp[i] == "0" or tmp[i] == "1":
-                datoAccp = True
-            else:
-                datoAccp = False
-                
-        if datoAccp == False:       
-            print("Error")
-            numero = input("Introduce un número binario válido(Ejemplos: 10100101b):")
-    print(conversorBinarioDecimal(numero))     
-
-elif numero[len(numero)-1:len(numero)] == "d":
-    numero = int(numero[:-1])
-    while numero < 0 :
-        numero = input("Introduce un número decimal positivo(Ejemplos: 1243):")
- 
-    print(conversorDecimalBinario(numero))     
-
-else:
-    print("Dato incorrecto")
+finalizado=False
+while finalizado==False:
+    if numero[len(numero)-1:len(numero)] == "b":
+        while datoAccp == False:
+            tmp = str(numero[:-1])
+            for i in range(len(tmp)):
+                if tmp[i] == "0" or tmp[i] == "1":
+                    datoAccp = True
+                else:
+                    datoAccp = False
+   
+            if datoAccp == False:
+                print("Error")
+                numero = input("Introduce un número binario válido(Ejemplo: 10100101b):")
+        print(conversorBinarioDecimal(tmp))
+        finalizado=True
+       
+    elif numero[len(numero)-1:len(numero)] == "d":
+        numero = int(numero[:-1])
+        while numero < 0 :
+            numero = input("Introduce un número decimal positivo(Ejemplo: 1243):")
+     
+        print(conversorDecimalBinario(numero))
+        finalizado=True
+       
+    else:
+        print("Dato incorrecto")
+        numero = input("Introduce un número decimal o binario(Ejemplos: 10100101b, 123d):")
 
 """
 
